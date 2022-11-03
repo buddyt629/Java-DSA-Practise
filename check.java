e79a0dab5daae72de9e0b70461bd7214d4bb0af4
buddyt629 <buddyt629@gmail.com>
2022-11-03 19:24:51 +0530

    main

---
+++ check.java
@@ -0,0 +1,8 @@
+import java.util.LinkedList;
+
+public class check {
+public static void main(String[] args) {
+    
+    LinkedList ll=new LinkedList<>();
+
+}}
---
+++ reverselinkedlist.java
@@ -0,0 +1,18 @@
+public class reverselinkedlist {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;  
+        ListNode current = head;
+    
+        
+        while(current != null) { 
+            ListNode next = current.next; 
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+       return prev; 
+    }
+    public static void main(String[] args) {
+        
+    }
+}
