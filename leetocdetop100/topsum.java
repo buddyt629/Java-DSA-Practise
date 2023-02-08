4840bfe9b810b7580171f941d29b13476603848d
buddyt629 <buddyt629@gmail.com>
2023-02-08 19:20:55 +0530

    practise

---
+++ leetocdetop100/topsum.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+public class topsum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+
+        for(int i=0;i<nums.length;i++)
+        {
+            if(map.containsKey(target-arr[i]))
+            {
+                return i;
+            }
+            else 
+            {
+                map.put(i, arr[i]);
+            }
+        }
+    }
+    public static void main(String[
+        6+-
+    ] args) {
+        
+    }
+}
