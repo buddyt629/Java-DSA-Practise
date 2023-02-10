bb6f92c076729cf58563457215ad95ee08177dba
buddyt629 <buddyt629@gmail.com>
2023-02-10 15:31:49 +0530

    practise

---
+++ leetocdetop100/Arrays/productExceptSelf.java
@@ -0,0 +1,8 @@
+class  productExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+        int left[]=new int[nums.length]
+    }
+    public static void main(String[] args) {
+        
+    }
+}
